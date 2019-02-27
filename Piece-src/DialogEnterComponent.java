de9a2a414213f71cade9b86b6c40603ebb16e133
wdj20101010 <wdj20101010@aliyun.com>
2019-02-27 19:32:08 +0800

    Create DialogEnterComponent.java

---
+++ Piece-src/DialogEnterComponent.java
@@ -0,0 +1,116 @@
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenuItem;
+
+@SuppressWarnings("serial")
+public class DialogEnterComponent extends JDialog {
+
+	String 输入内容="";
+
+	public DialogEnterComponent(String str) {
+		
+		setModal(true);
+		setTitle("\u6570\u636E\u8F93\u5165");
+		setBounds(100, 100, 640, 480);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setText(str);
+		scrollPane.setViewportView(textArea);
+		textArea.setFont(new Font("Monospaced", Font.BOLD, 16));
+		
+		JPopupMenu popupMenu = new JPopupMenu();
+		addPopup(textArea, popupMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("\u526A\u8D34");
+		popupMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("\u590D\u5236");
+		popupMenu.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("\u7C98\u8D34");
+		popupMenu.add(mntmNewMenuItem_2);
+		
+		mntmNewMenuItem.addActionListener(new ActionListener() {  //剪切
+			public void actionPerformed(ActionEvent e) {textArea.cut();}
+		});
+		
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {  //复制
+			public void actionPerformed(ActionEvent e) {textArea.copy();}
+		});
+		
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {  //粘贴
+			public void actionPerformed(ActionEvent e) {textArea.paste();}
+		});
+		
+		JButton btnNewButton = new JButton("\u786E\u5B9A");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {  //确定
+				输入内容=textArea.getText();setVisible(false);
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("\u53D6\u6D88");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {  //取消
+				System.exit(0);
+			}
+		});
+		
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnNewButton)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnNewButton_1)
+					.addContainerGap())
+				.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 399, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton_1)
+						.addComponent(btnNewButton))
+					.addContainerGap())
+		);
+		getContentPane().setLayout(groupLayout);
+		
+		setVisible(true);
+	}
+	
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+}
