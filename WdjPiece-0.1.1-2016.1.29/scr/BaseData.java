c9043600e3c1cec9629e78a5b5789ec685a01e0f
wdj20101010 <wdj20101010@aliyun.com>
2019-02-27 19:49:13 +0800

    Delete BaseData.java

--- WdjPiece-0.1.1-2016.1.29/scr/BaseData.java
+++
@@ -1,8 +0,0 @@
-//基本数据结构
-public class BaseData {
-	String 名称="";  //变量名称
-	byte 类型=0;  //变量当前类型    0字符串    1数字    2布尔
-	String 串值="";  //字符串类型的值
-	double 数值=0;  //数字类型的值
-	boolean 布值=false;  //布尔类型的值
-}
